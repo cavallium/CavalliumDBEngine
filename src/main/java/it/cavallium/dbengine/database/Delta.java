f95a46b6349241782d707bd8ea2da2d49307ed17
Andrea Cavalli <andrea@cavallium.it>
2021-06-01 17:08:37 +0200

    Add delta isModified

--- src/main/java/it/cavallium/dbengine/database/Delta.java
+++ src/main/java/it/cavallium/dbengine/database/Delta.java
@@ -1,5 +1,11 @@
 package it.cavallium.dbengine.database;
 
+import java.util.Objects;
 import org.jetbrains.annotations.Nullable;
 
-public record Delta<T>(@Nullable T previous, @Nullable T current) {}
+public record Delta<T>(@Nullable T previous, @Nullable T current) {
+
+	public boolean isModified() {
+		return !Objects.equals(previous, current);
+	}
+}

0e9b45ebf9772cc7e49aa7b42353a95656cdd15f
Andrea Cavalli <andrea@cavallium.it>
2022-06-04 16:33:45 +0200

    Update hitkey

--- src/main/java/it/cavallium/dbengine/client/HitKey.java
+++ src/main/java/it/cavallium/dbengine/client/HitKey.java
@@ -1,14 +1,24 @@
 package it.cavallium.dbengine.client;
 
+import it.cavallium.dbengine.database.collections.DatabaseEmpty.Nothing;
 import java.util.Comparator;
 import java.util.function.Function;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import reactor.core.publisher.Mono;
 
 public record HitKey<T>(T key, float score) implements Comparable<HitKey<T>> {
 
 	public <U> Mono<HitEntry<T, U>> withValue(Function<T, Mono<U>> valueGetter) {
 		return valueGetter.apply(key).map(value -> new HitEntry<>(key, value, score));
+	}
+
+	public <U> HitEntry<T, U> withNullValue() {
+		return new HitEntry<>(key, null, score);
+	}
+
+	public HitEntry<T, Nothing> withNothingValue() {
+		return new HitEntry<>(key, Nothing.INSTANCE, score);
 	}
 
 	@Override

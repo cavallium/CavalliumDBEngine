faa2a715979ea30699ed26767f0d793116689088
Andrea Cavalli <andrea@cavallium.it>
2022-07-29 00:33:23 +0200

    Fix compilation errors

--- src/main/java/it/cavallium/dbengine/database/disk/RocksIterWithReadOpts.java
+++ src/main/java/it/cavallium/dbengine/database/disk/RocksIterWithReadOpts.java
@@ -1,6 +1,7 @@
 package it.cavallium.dbengine.database.disk;
 
 import it.cavallium.dbengine.database.DiscardingCloseable;
+import it.cavallium.dbengine.database.LLUtils;
 import it.cavallium.dbengine.database.SafeCloseable;
 import it.cavallium.dbengine.database.disk.rocksdb.RocksIteratorObj;
 import org.rocksdb.ReadOptions;
@@ -9,7 +10,7 @@ public record RocksIterWithReadOpts(ReadOptions readOptions, RocksIteratorObj it
 
 	@Override
 	public void close() {
-		if (readOptions != null && readOptions.isAccessible()) {
+		if (readOptions != null && LLUtils.isAccessible(readOptions)) {
 			readOptions.close();
 		}
 		iter.close();
